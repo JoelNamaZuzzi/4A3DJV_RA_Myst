70e226efe01727361ca157cbc4264f644b8cefa5
JoelNamaZuzzi <59820667+JoelNamaZuzzi@users.noreply.github.com>
2022-02-27 00:57:08 +0100

    Working Simple button puzzle

--- Assets/Script/ThirdMyst/ButtonTouch.cs
+++ Assets/Script/ThirdMyst/ButtonTouch.cs
@@ -8,15 +8,28 @@ public class ButtonTouch : MonoBehaviour
     public Camera cam;
     public Material RedButtonMat;
     public Material BlackButtonMat;
-
+    public Material IndiceMat;
     
+    private int score = 0;
+    private bool b1=false;
+    private bool b2=false;
+    private bool b3=false;
+    private bool b4=false;
+    private bool b5=false;
+    private bool b6=false;
+
+    public GameObject Light1;
+    public GameObject Light2;
+    public GameObject Light3;
+
+    public GameObject Indice;
     void Update()
     {
         //if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
         if (Input.GetMouseButtonDown(0))
         {
             //Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            
+
             RaycastHit Hit;
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out Hit))
@@ -25,26 +38,58 @@ public class ButtonTouch : MonoBehaviour
                 switch (buttonName)
                 {
                     case ("Button1"):
-                        Debug.Log("Bite! 1");
+                        Debug.Log("bite");
+                        b1=ARButtonClick(Hit, 5, b1);
+                        Light1.SetActive(!Light1.activeSelf);
                         break;
                     case ("Button2"):
-                        Debug.Log("Bite! 2");
+                        b2=ARButtonClick(Hit, 10, b2);
+                        Light1.SetActive(!Light1.activeSelf);
+                        Light3.SetActive(!Light3.activeSelf);
                         break;
                     case ("Button3"):
-                        Debug.Log("Bite! 3");
+                        b3=ARButtonClick(Hit, 15, b3);
+                        Light2.SetActive(!Light2.activeSelf);
+                        Light3.SetActive(!Light3.activeSelf);
                         break;
                     case ("Button4"):
-                        Debug.Log("Bite! 4");
+                        b4=ARButtonClick(Hit, 20, b4);
+                        Light1.SetActive(!Light1.activeSelf);
+                        Light2.SetActive(!Light2.activeSelf);
                         break;
                     case ("Button5"):
-                        Debug.Log("Bite! 5");
+                        b5=ARButtonClick(Hit, 25, b5);
+                        Light2.SetActive(!Light2.activeSelf);
                         break;
                     case ("Button6"):
-                        Debug.Log("Bite! 6");
+                        b6=ARButtonClick(Hit, 30, b6);
+                        Light3.SetActive(!Light3.activeSelf);
                         break;
-
                 }
+                
+                if(Light1.activeSelf && Light2.activeSelf && Light3.activeSelf)
+                {
+                    Indice.GetComponent<Renderer>().material = IndiceMat;
+                }
+                
+                
             }
+        }
+    }
+
+    private bool ARButtonClick(RaycastHit button, int val, bool bt)
+    {
+        if (bt == true)
+        {
+            button.transform.gameObject.GetComponent<Renderer>().material = BlackButtonMat;
+            score -= val;
+            return false;
+        }
+        else
+        {
+            button.transform.gameObject.GetComponent<Renderer>().material = RedButtonMat;
+            score += val;
+            return true;
         }
     }
 }
