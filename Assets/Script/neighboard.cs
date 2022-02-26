4922e23a48826a70a5f478a1d7712e413a1735ff
Rezay <rezaygame@gmail.com>
2022-02-26 22:02:31 +0100

    MiniGame Operationnel

---
+++ Assets/Script/neighboard.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class neighboard : MonoBehaviour
+{
+    [SerializeField]private neighboardDetector detecteurGauche;
+    [SerializeField]private neighboardDetector detecteurDroite;
+    
+    [Header("RealTime Value")]
+    public neighboard voisinDeDroite;
+    public neighboard voisinDeGauche;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        voisinDeDroite = detecteurDroite.info;
+        voisinDeGauche = detecteurGauche.info;
+    }
+    
+}
